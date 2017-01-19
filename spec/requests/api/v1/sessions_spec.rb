acec6afc0303abb47f2031fdb91ff37241f96d7f
Philip <Philip.zdb@gmail.com>
2017-01-19 15:51:41 +0100

    Adds rspec test for session request with invalid email

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -25,10 +25,20 @@ RSpec.describe 'Sessions', type: :request do
       expect(response.status).to eq 200
     end
 
-    it 'invalid password reutrns error message' do
+    it 'invalid password returns error message' do
       post '/api/v1/auth/sign_in', params: {
           email: user.email,
           password: 'wrong_password'
+      }, headers: headers
+
+      expect(response_json['errors']).to eq ["Invalid login credentials. Please try again."]
+      expect(response.status).to eq 401
+    end
+
+    it 'invalid email returns error message' do
+      post '/api/v1/auth/sign_in', params: {
+          email: 'wrong@email.com',
+          password: user.password
       }, headers: headers
 
       expect(response_json['errors']).to eq ["Invalid login credentials. Please try again."]
