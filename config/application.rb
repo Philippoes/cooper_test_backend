5294b19654d2deabcc27bc3aef8655f6703a1808
Philip <Philip.zdb@gmail.com>
2017-01-19 20:34:07 +0100

    Adds config in application.rb to check token expiry

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,10 @@ module CooperTestBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :delete]
+        resource '*',
+                 headers: :any, methods: [:get, :post, :put, :delete],
+                 expose: %w(access-token expiry token-type uid client),
+                 max_age: 0
       end
     end
   end
