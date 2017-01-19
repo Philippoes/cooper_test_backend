6227a670ebaaee6b008627d2fb00ed9c5247521e
Philip <Philip.zdb@gmail.com>
2017-01-19 16:01:01 +0100

    Adds DB table spec for PerformanceData

--- spec/models/performance_data_spec.rb
+++ spec/models/performance_data_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe PerformanceData, type: :model do
-  
+  describe 'Database table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :data }
+    it { is_expected.to have_db_column :created_at }
+    it { is_expected.to have_db_column :updated_at }
+  end
 end
