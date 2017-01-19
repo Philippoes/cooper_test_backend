598f8cc5d99373a1736b0455b7b90b444cb0e0b9
Philip <Philip.zdb@gmail.com>
2017-01-19 13:44:32 +0100

    Fixes a bug in a user spec, added not_to instead of to, formats code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,10 +36,17 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:password) }
 
     context 'should not have an invalid email address' do
-      emails = ['asdf@ ds.com', '@example.com', 'test me @yahoo.com', 'asdf@example','ddd@.d.', 'ddd@.d']
+      emails = [
+          'asdf@ ds.com',
+          '@example.com',
+          'test me @yahoo.com',
+          'asdf@example',
+          'ddd@.d.',
+          'ddd@.d'
+      ]
 
       emails.each do |email|
-        it { is_expected.to allow_value(email).for(:email)}
+        it { is_expected.not_to allow_value(email).for(:email) }
       end
     end
   end
