c23c3f7f61e0868943c52b3ffe94d773af1de35a
Philip <Philip.zdb@gmail.com>
2017-01-19 14:31:50 +0100

    Adds ResponseJSON module to translate ruby response to JSON, also modifies ping_spec to use that action

--- spec/requests/api/v0/ping_spec.rb
+++ spec/requests/api/v0/ping_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe Api::V0::PingController, type: :request do
   describe 'GET /v0/ping' do
     it 'should return pong' do
       get '/api/v0/ping'
-
-      json_response = JSON.parse(response.body)
+      
       expect(response.status).to eq 200
-      expect(json_response['message']).to eq 'Pong'
+      expect(response_json['message']).to eq 'Pong'
     end
   end
 end
