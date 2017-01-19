53babf5ce3ec556a6364685d186429501b7fa98f
Philip <Philip.zdb@gmail.com>
2017-01-19 20:09:41 +0100

    Fixes the controller, needs whitelist to make test pass

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::PerformanceDataController < ApplicationController
   def create
-    Performance_data.create(data: params[:performance_data]).valid?
+    binding.pry
+    PerformanceData.create(params[:performance_data]).valid?
     render json: { message: 'all good' }
   end
 end
