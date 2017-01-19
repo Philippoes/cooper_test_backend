4b27357595a9a3a403f1420961893b986f6a6147
Philip <Philip.zdb@gmail.com>
2017-01-19 20:19:17 +0100

    Rewrites the PerformanceData controller

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::PerformanceDataController < ApplicationController
+  before_action :authenticate_api_v1_user!
+
   def create
     binding.pry
-    PerformanceData.create(params[:performance_data]).valid?
-    render json: { message: 'all good' }
+    @data = PerformanceData.create(performance_data_params.merge(user: current_api_v1_user)).valid?
+    render json: {message: 'all good'}
+  else
+    render json: {error: @data.errors.full_messages}
+    end
+
+  private
+
+  def performance_data_params
+    params.require(:performance_data).permit!
   end
 end
