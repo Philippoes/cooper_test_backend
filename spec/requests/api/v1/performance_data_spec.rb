7cb7010f90f3e751045e5b79fe4d52016e9e8634
Philip <Philip.zdb@gmail.com>
2017-01-20 08:26:14 +0100

    Adds spec for post request from user with no token, and user not logged in

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -12,5 +12,26 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
       entry = PerformanceData.last
       expect(entry.data).to eq 'message' => 'Average'
     end
+
+    it 'gives error when user has no token' do
+      post '/api/v1/performance_data', params: {
+          performance_data: {data: {message: 'Average'}}
+      }, headers: nil
+
+      expect(response_json['errors']).to eq ["Authorized users only."]
+      expect(response.status).to eq 401
+    end
+
+    describe 'User is not logged in' do
+      let(:headers) { {HTTP_ACCEPT: 'application/json'} }
+      it 'gives error when user is not logged in' do
+        post '/api/v1/performance_data', params: {
+            performance_data: {data: {message: 'Average'}}
+        }, headers: headers
+
+        expect(response_json['errors']).to eq ["Authorized users only."]
+        expect(response.status).to eq 401
+      end
+    end
   end
 end
